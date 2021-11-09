41f34637b139ea437b0b4a780371fbee911e6d89
20jy0215 <20jy0215@PC09.jynet2.jec.ac.jp>
2021-11-09 16:00:58 +0900

    円の色を青から赤に変更

--- src/ez/DrawFigure.java
+++ src/ez/DrawFigure.java
@@ -35,8 +35,9 @@ public class DrawFigure {
 		EZ.addText(40,20, "Hello World", Color.BLACK, 12);
 		EZ.addText(300,50, "012あいう漢字", Color.BLACK, 24);
 
-		// 円を描く．(中心座標x, 中心座標y, 幅，高さ，色，塗りつぶし)
-		EZ.addCircle(100, 100, 150, 150, Color.BLUE, true);
-		EZ.addCircle(200, 200, 200, 200, Color.RED,  false);
+		// 円を描く.(中心座標 x, 中心座標 y, 幅,高さ,色,塗りつぶし)
+		EZCircle circle = EZ.addCircle(100, 100, 150, 150, Color.BLUE, true);
+		circle.setColor(Color.RED);
+		
 	}
 }
